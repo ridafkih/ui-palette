3aa12772e0f437f8d040949b98ccb67bea5142a4
Rida Fakih <ridafakih@gmail.com>
2021-04-26 18:01:48 -0600

    updated initial z

--- public/src/script.ts
+++ public/src/script.ts
@@ -51,32 +51,22 @@ scene.add(pointLight);
 
 // ** OBJECT CONFIGURATION
 
-const state = {
-  position: {
-    x: 0,
-    y: 0.25,
-    z: -150,
-  },
-  rotation: {
-    x: 0,
-    y: 0,
-    z: 0,
-  },
-};
-
-document.addEventListener("mousemove", ({ clientX, clientY }) => {
-  const xMidRef = (clientX - window.innerWidth / 2) / window.innerWidth;
-  const yMidRef = (clientY - window.innerHeight / 2) / window.innerHeight;
-
-  state.rotation.y = xMidRef * 0.5;
-  state.rotation.x = yMidRef * 0.5;
-});
-
 (async () => {
   const model: Object3D = await loadObject(
     scene,
     "./models/iphone_12_pro/model.glb"
   );
+
+  const reference = objectStateHandler.add(model, "iPhone");
+  reference.state.position.z = -150;
+
+  document.addEventListener("mousemove", ({ clientX, clientY }) => {
+    const xMidRef = (clientX - window.innerWidth / 2) / window.innerWidth;
+    const yMidRef = (clientY - window.innerHeight / 2) / window.innerHeight;
+  
+    reference.state.rotation.y = xMidRef * 0.5;
+    reference.state.rotation.x = yMidRef * 0.5;
+  });
 
   applyTextureToChild(model, "Screen_Wallpaper_0", "./texture/home.png");
 
@@ -84,12 +74,7 @@ document.addEventListener("mousemove", ({ clientX, clientY }) => {
 
   function animate() {
     requestAnimationFrame(animate);
-
-    const { position, rotation } = state;
-
-    model.position.set(position.x, position.y, position.z);
-    model.rotation.set(rotation.x, rotation.y, rotation.z);
-
+    objectStateHandler.updateObjects();
     renderer.render(scene, camera);
   }
 
